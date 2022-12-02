2857e8ff5ad1f3e1c681ad553a37110499fc1bc1
nisbansa <nisbansa@adobe.com>
2022-12-02 17:37:57 +0530

    fix-constants

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,5 @@
 /*jslint vars: true, unparam: true, plusplus: true, node: true */
 /*global require */
 
-export const API_PREFIX = '/apoconsole';
-export const API_SUFFIX = '?api_key=asrapikey';
-export const profilePictureBaseUrl = 'https://s7d2.scene7.com/is/image/IMGDIR/';
+module.exports.API_PREFIX = '/apoconsole';
+module.exports.API_SUFFIX = '?api_key=asrapikey';
