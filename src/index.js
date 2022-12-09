f7f173a9e332dc278519f135ef4f894945ccc8b6
Arun Snghal <asnghal@adobe.com>
2022-12-09 17:49:18 +0530

    commited

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {React} from 'react';
 import MakeHeader from './components/MakeHeader';
 import MakeCard from './components/MakeCard';
 import ReactDOM from 'react-dom';
+import Login from './components/Login';
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <App />
+    <Login />
     // <MakeCard/>
   );
