ad19bb30ecf4ffaac0c33cce909cb55c8b1e2302
Arun Snghal <asnghal@adobe.com>
2022-12-09 18:20:53 +0530

    commited

---
+++ src/App1.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import TabBar from './components/TabBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MakeHeader from './components/MakeHeader';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import MakeCard from './components/MakeCard';
+import MakeLabel  from './components/MakeLabel';
+import Login from './components/Login';
+
+function App1() {
+  return (
+    <div>
+ <Router>
+
+  <Routes>
+  <Route exact path='/login' element={ <Login/>} >  </Route> 
+  </Routes>
+
+      {/* <Header/> */}
+
+      <Routes>
+         <Route exact path='/home' element={  < MakeCard />}></Route> 
+         <Route exact path='/email-suppresion' element={< MakeLabel/>}>  </Route>  
+         <Route exact path='/bulk-email-sent' element={< MakeHeader/>}>  </Route>
+        
+         
+          </Routes>
+     </Router>  
+
+    </div>
+  );
+}
+
+export default App1;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import MakeHeader from './components/MakeHeader';
 import MakeCard from './components/MakeCard';
 import ReactDOM from 'react-dom';
 import Login from './components/Login';
+import App1 from './App1';
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  
-    <Login/>
+    <App1/>
     // <MakeCard/>
   );
