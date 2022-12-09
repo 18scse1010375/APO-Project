481964b9ff2b631fe585549e5913a344a095029a
Arun Snghal <asnghal@adobe.com>
2022-12-09 18:07:45 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,31 @@
 import React from "react";
 import {Button, defaultTheme, Provider} from '@adobe/react-spectrum';
 import  LoginStyle from  './LoginStyle.css';
+import { adobeIms, logout } from '.\\services\\AdobeIms';
+
+const signIn = ()=> {
+  adobeIms.signIn();
+}
+
+const signOut = () => {
+  logout();
+}
 
 const Login=()=>{
     return(
         <div id="login-page" className="text-center" >
 
-      {/* style={{marginTop:'170px'} */}
+      
             <h2 className="apo">APO Console Management Center</h2>
-            {/* style={{fontSize:'2.25rem'} */}
+           
 
             <p>Welcome to APO Console.You can discover,create and manage communications across email,notification and web</p>
 
               <Provider theme={defaultTheme}>
                 <div className="sign-in">
-                {/* style={{width:'100%',color:'ye'} */}
-              <Button id="button"   tag='a' href="/user" variant="accent"     onPress={() => alert('Hey there!')} >   Sign In  </Button>
+                
+              <Button onClick={signIn} id="button"   tag='a' href="/user" variant="accent"  >   Sign In  </Button>
+              <Button onClick={signOut} id="button"   tag='a' href="/user" variant="accent"  >   Sign Out  </Button>
               </div>
               </Provider>
               </div>
