603ea9196fb87af432bf5afdaaa87d23ea65fd7f
Venkatesh Babu <venkkris@adobe.com>
2023-01-17 19:17:26 +0530

    Add console logs for environment variables

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { React } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
- import App from './App';
-//import Sample from 'Sample';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  
 );
 
