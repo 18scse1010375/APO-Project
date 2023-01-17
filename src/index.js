e50a1aa491b017b20f9ad60ffb96115fdfadac98
Venkatesh Babu <venkkris@adobe.com>
2023-01-17 16:06:31 +0530

    Revert "add extra component for checking commited"
    This reverts commit 075900dbafe82bbb1e6e5831e4728eaa02651f06.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import { React } from 'react';
 import ReactDOM from 'react-dom/client';
  import App from './App';
 //import Sample from 'Sample';
-import Testing from './components/Testing';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Testing/>
+  <App />
 );
 
